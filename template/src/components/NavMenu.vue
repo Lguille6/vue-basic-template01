a37493a2f2a89621c8fb7fbdce02b2bc86f95be0
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-02-23 21:57:17 -0600

    Finished template

---
+++ template/src/components/NavMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex justify-center gap-4">
+    <RouterLink
+      to="/"
+      class="[&.router-link-active]underline hover:underline hover:underline-offset-4 [&.router-link-active]:underline-offset-4"
+    >
+      Home
+    </RouterLink>
+    <RouterLink
+      to="/other"
+      class="[&.router-link-active]underline hover:underline hover:underline-offset-4 [&.router-link-active]:underline-offset-4"
+    >
+      Other
+    </RouterLink>
+  </nav>
+</template>
