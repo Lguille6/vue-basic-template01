c8814ebcd8db90beb8fed53bf65a762069f80d74
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-02-24 19:11:14 -0600

    Added basic layout to LinkCard

--- template/src/components/LinkCard.vue
+++ template/src/components/LinkCard.vue
@@ -1,3 +1,36 @@
 <template>
-  <p>This is a link card.</p>
+  <a
+    href="#"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="flex w-full items-center gap-4 rounded-xl border border-pink-500 bg-pink-100 p-4 shadow-md shadow-pink-900/50 transition-all duration-300 hover:scale-[1.02] hover:border-pink-700 hover:shadow-xl hover:shadow-pink-500/20"
+  >
+    <!-- Icon Container-->
+    <div
+      class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-pink-200 text-pink-400"
+    >
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        class="h-6 w-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 20c-2.205-.48-9-4.24-9-11a5 5 0 0 1 9-3a5 5 0 0 1 9 3c0 6.76-6.795 10.52-9 11m0-2c3.12-.93 7-4.805 7-9a3 3 0 0 0-3-3q-1.957 0-4 2.5Q9.957 6 8 6a3 3 0 0 0-3 3c0 4.195 3.88 8.07 7 9"
+        />
+        />
+      </svg>
+    </div>
+
+    <!-- Text Content-->
+    <div class="min-w-0 flex-1">
+      <h3 class="truncate font-display font-semibold text-pink-800">Link Title</h3>
+      <p class="truncate text-sm text-pink-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+  </a>
 </template>
