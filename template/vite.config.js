df71fbb3afff48c4503f306ff7904343a7bc158e
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-02-24 22:24:54 -0600

    Added wrangler deployment

--- template/vite.config.js
+++ template/vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import tailwindcss from '@tailwindcss/vite'
 
 import { cloudflare } from '@cloudflare/vite-plugin'
